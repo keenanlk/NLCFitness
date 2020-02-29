b65d12e892691ecfb1659cec34aca53c2dd30d94
Keenan Kaufman <keenanlkaufman@gmail.com>
2020-02-29 14:34:33 -0600

    delete

--- src/app/credentials.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBxwA8WE5vIbE5OZr_FgzJN0LjY7BqnZAA",
-  authDomain: "nlc-fitness.firebaseapp.com",
-  databaseURL: "https://nlc-fitness.firebaseio.com",
-  projectId: "nlc-fitness",
-  storageBucket: "nlc-fitness.appspot.com",
-  messagingSenderId: "944019642075",
-  appId: "1:944019642075:web:cb2d2dbf0baa2c5b0c1efe"
-};
